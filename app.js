3f535b2d7a0280c6f0e493f2e83f3ae5ac59f1f9
Antonius Matheus <matheus.r.g@hotmail.com>
2025-03-20 20:04:47 -0300

    Ativando o botao Reiniciar

--- app.js
+++ app.js
@@ -21,9 +21,31 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    alterarStatusBotao();
 }
 
 // Funcao para achar um valor entre dois numeros, incluindo o ultimo possível
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Funcao condicional para mudar class do botao "reiniciar" de desabilitado para habilitado
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+// Funcao para reiniciar os valores e recomecar o sorteio
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
 }
